63b3aee64976ffa4537f4aaa686d0d7bd197e859
Eric Stiles <ets04uga@yahoo.com>
2014-08-04 11:58:36 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,80 @@
+# MetalSmith-Cloud
+
+A (metalsmith)[https://github.com/segmentio/metalsmith] plugin that creates an array of weighted values for use in a word cloud.
+
+This plugin needs to be used in conjunction with (metalsmith-tags)[https://github.com/totocaster/metalsmith-tags] as the path for the cloud category can used to point to the html page listing of related articles.  The metalsmith-tags plugin takes the string list of categories/tags and converts them to an array of values.  The metalsmith-cloud plugin is dependent on this array that exists in each file object.
+
+Cloud tags are sorted alphanumerically by tag name.
+
+## Installation
+
+    $ npm install metalsmith-cloud --save-dev
+
+## CLI Usage
+
+  Install the node modules and then add the `metalsmith-tags` key to your `metalsmith.json` plugins. The simplest use case just requires tag handle you want to use:
+
+```json
+{
+  "plugins": {
+    "metalsmith-cloud": {
+      "handle": "tags",
+      "path": "topics",
+      "reverse": true
+    }
+  }
+}
+```
+
+## Javascript Usage
+
+  Pass the plugin to `Metalsmith#use`:
+
+```js
+var tags = require('metalsmith-tags'),
+wordcloud = require('metalsmith-cloud');
+
+metalsmith
+    .use(tags({
+        handle: 'tags',
+        path: 'topics',
+        template: '/partials/tag.hbt'
+    }))
+    .use(wordcloud({
+        category: 'tags', //optional, default is tags
+        reverse: false, //optional sort value on category, default is false
+        path: 'topics'
+    }))
+```
+
+## Usage
+
+### Under The Covers
+A property key 'cloud' is added to the metadata containing a sorted array of objects with 'ctg', 'wght' and 'pth' keys.
+
+```json
+    {cloud: 
+    [ { ctg: 'aws', wght: 1, pth: '/topics/aws' },
+     { ctg: 'bash', wght: 1, pth: '/topics/bash' },
+     { ctg: 'blog', wght: 2, pth: '/topics/blog' } ] }
+```
+
+### Application
+
+In (handlebars)[] the following example creates links for each category.
+
+    {{#each cloud}}
+        <a href="{{this.pth}}">
+            {{this.ctg}}({{this.wght}})
+        </a>
+    {{/each}}
+
+where the html built would be as follows:
+
+```html
+    <a href="/topics/aws">aws(1)</a>
+    <a href="/topics/bash">bash(1)</a>
+    <a href="/topics/blog">blog(1)</a>
+```
+
+There are several implementations for creating word clouds.  [http://ericstiles.github.io] uses (jquery.tagcloud.js)[https://github.com/addywaddy/jquery.tagcloud.js] to create a wordcloud on the site.
---
+++ test/index.js
@@ -0,0 +1,185 @@
+var assert = require('assert'),
+    equal = require('assert-dir-equal'),
+    tags = require('..'),
+    cloud = require('../lib/index.js');
+
+//mock metalsmith object
+var metalsmith = {
+    data: {
+        "dir": "/Users/ericstiles/repo/metalsmith-wordcloud",
+        "ware": {
+            "fns": [null, null]
+        },
+        "data": {},
+        "_src": "src",
+        "_dest": "build",
+        "_clean": true,
+        "_frontmatter": true
+    },
+    metadata: function() {
+        return this.data;
+    }
+};
+
+//expected return array when no values provided to plugin 
+//and default values are used
+var expected_cloud_default_values = [{
+    ctg: 'cat1',
+    wght: 1,
+    pth: 'cat1'
+}, {
+    ctg: 'cat2',
+    wght: 1,
+    pth: 'cat2'
+}, {
+    ctg: 'cat3',
+    wght: 2,
+    pth: 'cat3'
+}];
+//expected return array when values provided to plugin
+var expected_cloud_provided_values = [{
+    ctg: 'cat1',
+    wght: 1,
+    pth: 'topics/cat1'
+}, {
+    ctg: 'cat2',
+    wght: 1,
+    pth: 'topics/cat2'
+}, {
+    ctg: 'cat3',
+    wght: 2,
+    pth: 'topics/cat3'
+}];
+// Mock file array
+var files = {
+    "content/posts/a.html": {
+        "title": "title-one",
+        "date": "2013-04-01T00:00:00.000Z",
+        "tags": ["cat1"],
+        "template": "blog.hbt",
+        "contents": [60, 112, 62, 116, 105, 116, 108, 101, 45, 111, 110, 101, 60, 47, 112, 62, 10],
+        "mode": "0644"
+    },
+    //Notice a bug in the metalsmith-tags plugin that creates in an empty array.  Underscore
+    //appears to take care of this issue.
+    "content/posts/b.html": {
+        "title": "title-two",
+        "date": "2013-04-01T00:00:00.000Z",
+        "tags": ["cat2", "cat3", ""],
+        "template": "blog.hbt",
+        "contents": [60, 112, 62, 116, 105, 116, 108, 101, 45, 116, 119, 111, 60, 47, 112, 62, 10],
+        "mode": "0644"
+    },
+    "content/posts/c.html": {
+        "title": "title-three",
+        "date": "2013-04-01T00:00:00.000Z",
+        "tags": ["cat3"],
+        "template": "blog.hbt",
+        "contents": [60, 112, 62, 116, 105, 116, 108, 101, 45, 116, 104, 114, 101, 101, 60, 47, 112, 62, 10],
+        "mode": "0644"
+    },
+    "content/posts/e.html": {
+        "title": "title-five",
+        "date": "2013-04-01T00:00:00.000Z",
+        "tags": null,
+        "template": "blog.hbt",
+        "contents": [60, 112, 62, 116, 105, 116, 108, 101, 45, 102, 105, 118, 101, 60, 47, 112, 62, 10],
+        "mode": "0644"
+    },
+    "content/posts/d.html": {
+        "title": "title-four",
+        "date": "2013-04-01T00:00:00.000Z",
+        "template": "blog.hbt",
+        "contents": [60, 112, 62, 116, 105, 116, 108, 101, 45, 102, 111, 117, 114, 60, 47, 112, 62, 10],
+        "mode": "0644"
+    },
+    "topics/cat1.html": {
+        "template": "/partials/tag.hbt",
+        "mode": "0644",
+        "contents": "",
+        "tag": "cat1",
+        "posts": [{
+            "title": "title-one",
+            "date": "2013-04-01T00:00:00.000Z",
+            "tags": ["cat1"],
+            "template": "blog.hbt",
+            "contents": [60, 112, 62, 116, 105, 116, 108, 101, 45, 111, 110, 101, 60, 47, 112, 62, 10],
+            "mode": "0644"
+        }]
+    },
+    "topics/cat2.html": {
+        "template": "/partials/tag.hbt",
+        "mode": "0644",
+        "contents": "",
+        "tag": "cat2",
+        "posts": [{
+            "title": "title-two",
+            "date": "2013-04-01T00:00:00.000Z",
+            "tags": ["cat2", "cat3", ""],
+            "template": "blog.hbt",
+            "contents": [60, 112, 62, 116, 105, 116, 108, 101, 45, 116, 119, 111, 60, 47, 112, 62, 10],
+            "mode": "0644"
+        }]
+    },
+    "topics/cat3.html": {
+        "template": "/partials/tag.hbt",
+        "mode": "0644",
+        "contents": "",
+        "tag": "cat3",
+        "posts": [{
+            "title": "title-two",
+            "date": "2013-04-01T00:00:00.000Z",
+            "tags": ["cat2", "cat3", ""],
+            "template": "blog.hbt",
+            "contents": [60, 112, 62, 116, 105, 116, 108, 101, 45, 116, 119, 111, 60, 47, 112, 62, 10],
+            "mode": "0644"
+        }, {
+            "title": "title-three",
+            "date": "2013-04-01T00:00:00.000Z",
+            "tags": ["cat3"],
+            "template": "blog.hbt",
+            "contents": [60, 112, 62, 116, 105, 116, 108, 101, 45, 116, 104, 114, 101, 101, 60, 47, 112, 62, 10],
+            "mode": "0644"
+        }]
+    },
+    "topics/.html": {
+        "template": "/partials/tag.hbt",
+        "mode": "0644",
+        "contents": "",
+        "tag": "",
+        "posts": [{
+            "title": "title-two",
+            "date": "2013-04-01T00:00:00.000Z",
+            "tags": ["cat2", "cat3", ""],
+            "template": "blog.hbt",
+            "contents": [60, 112, 62, 116, 105, 116, 108, 101, 45, 116, 119, 111, 60, 47, 112, 62, 10],
+            "mode": "0644"
+        }]
+    }
+};
+//Mock metalsmith done function called when plugin finishes.
+var testDone = function() {};
+
+describe('metalsmith-cloud using default options by not providing an options parameter', function() {
+    it('should create a new key in metadata object for a sorted array of weighted categories with links', function(done) {
+        var plugin = cloud();
+        plugin(files, metalsmith, testDone);
+        assert.notEqual(metalsmith.metadata().cloud, undefined, "cloud key doesn't exist in metadata");
+        assert.deepEqual(metalsmith.metadata().cloud, expected_cloud_default_values);
+        done();
+    });
+});
+
+describe('metalsmith-cloud using user provided configurations', function() {
+    it('should create a new key in metadata object for a sorted array of weighted categories with links', function(done) {
+        var plugin = cloud({
+            category: 'tags',
+            reverse: false, //default is false
+            path: 'topics'
+        });
+        plugin(files, metalsmith, testDone);
+        assert.notEqual(metalsmith.metadata().cloud, undefined, "cloud key doesn't exist in metadata");
+        assert.deepEqual(metalsmith.metadata().cloud, expected_cloud_provided_values);
+        done();
+    });
+});
